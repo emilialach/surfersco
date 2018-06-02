63fb4474d6056dfed5b92e23944a25db89acda35
emilialach <produkty.tash@gmail.com>
2018-06-02 15:34:18 +0200

    Slick, sticky header,  scripts

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     // When the user scrolls the page, execute myFunction 
+//     window.onscroll = function () {
+//         onScrollSticky()
+//     };
+//
+//     // Get the header
+//     var header = document.getElementById("myHeader");
+//
+//     var menu = document.getElementById("header");
+//
+//     // Get the offset position of the navbar
+//     var sticky = menu.offsetWidth;
+//     console.log(sticky);
+// });
